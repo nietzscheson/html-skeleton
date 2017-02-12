017b4a751dd06254534a6ad2aca993230f955ee6
Cristian Angulo Nova <cristianangulonova@gmail.com>
2017-02-12 16:32:23 +0000

    Add tasks: minify, copy, html replace

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,12 @@
         reload = browserSync.reload,
         sass = require('gulp-sass'),
         cleanCSS = require('gulp-clean-css'),
-        uglify = require('gulp-uglify');
+        uglify = require('gulp-uglify'),
+        plumber = require('gulp-plumber'),
+        concat = require('gulp-concat'),
+        rename = require('gulp-rename'),
+        copy = require('gulp-copy'),
+        htmlreplace = require('gulp-html-replace');
 
     gulp.task('browser-sync', function(){
 
@@ -16,20 +21,60 @@
             port: 4444
         });
 
-        gulp.watch(['dist/*.css', '**/*.js', '**/*.html', '!gulpfile.js']).on('change', reload);
-
-        gulp.watch('styles/*.sass', ['sass']);
-        gulp.watch('dist/*.css', ['minify']);
-
     });
+
+    function minifyCss(files, concatfiles, renamefile, destfile){
+      return gulp.src(files)
+          .pipe(concat(files))
+          .pipe(rename(renamefile))
+          .pipe(cleanCSS({compatibility: 'ie8'}))
+          .pipe(gulp.dest(destfile))
+    }
 
     gulp.task('minify-css', function(){
-        return gulp.src('styles/*.css')
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest('dist'))
+
+      var to = 'dist',
+          dest = to;
+
+      minifyCss('styles/*.css', to, 'skeleton.min.css', dest);
+      minifyCss('bower_components/**/*.css', to, 'vendors.min.css', dest);
     });
 
-    gulp.task('minify', ['minify-css']);
+    function minifyJs(files, concatfiles, renamefile, destfile){
+
+      return gulp.src(files)
+        .pipe(plumber())
+        .pipe(concat(concatfiles))
+        .pipe(rename(renamefile))
+        .pipe(uglify())
+        .pipe(gulp.dest(destfile));
+    }
+
+    gulp.task('minify-js', function(){
+
+        var to = 'dist',
+            dest = to;
+
+        minifyJs('scripts/*.js', to, 'skeleton.min.js', dest);
+        minifyJs('bower_components/**/*.min.js', to, 'vendors.min.js', dest);
+    });
+
+    gulp.task('minify', ['minify-css','minify-js']);
+
+    gulp.task('copy', function(){
+
+        return gulp.src('index.html')
+          .pipe(rename('index.min.html'))
+          .pipe(htmlreplace({
+                  'css': 'styles.min.css',
+                  'js': 'js/bundle.min.js'
+              }))
+          .pipe(gulp.dest('./'))
+        ;
+
+    });
+
+    gulp.task('build', ['minify','copy'], function(){});
 
     gulp.task('sass', function () {
         return gulp.src('styles/*.sass')
@@ -39,4 +84,11 @@
           ;
     });
 
-    gulp.task('default', ['browser-sync'], function(){});
+    gulp.task('watch', function(){
+
+      gulp.watch(['dist/*.css', '**/*.js', '**/*.html', '!gulpfile.js']).on('change', reload);
+      gulp.watch('styles/*.sass', ['sass']);
+
+    });
+
+    gulp.task('default', ['browser-sync','watch'], function(){});
